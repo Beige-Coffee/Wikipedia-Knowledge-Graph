c192507cefef3f1fc7b26ec34d3c81ac08f5a2ad
Austin Krauss <austin.f.krauss@gmail.com>
2018-08-27 15:06:57 -0700

    Build out website automation

--- my_jinja_app/wiki_app.py
+++ my_jinja_app/wiki_app.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 import numpy as np
 import pandas as pd
+import urllib.parse
 
 app = Flask(__name__,
             static_url_path='') 
@@ -14,21 +15,18 @@ def welcome_page():
     return render_template('home.html')
 
 
-@app.route('/machine_learning', methods=['GET'])
-def machine_learning_page():
-    return render_template('index.html')
-
-@app.route('/jinja', methods=['GET'])
-def load_table():
+@app.route('/category/<category_name>', methods=['GET'])
+def load_category(category_name):
     data = pickle.load( open( "/Users/austin/Documents/Galvanize/Capstone/Wikipedia_Knowledge_Graph/EDA/ML_data.pkl", "rb" ) )
-    return render_template('index.html', data = data)
+    data['url_cat'] = data['category'].apply(urllib.parse.quote)
+    return render_template('index.html', data=data, category_name=category_name)
 
-@app.route('/applied', methods=['GET'])
-def load_table2():
+@app.route('/sub_category/<sub_category_name>', methods=['GET'])
+def load_sub_category(sub_category_name):
     data = pickle.load( open( "/Users/austin/Documents/Galvanize/Capstone/Wikipedia_Knowledge_Graph/EDA/ML_pages.pkl", "rb" ) )
-    data = data[data['category'] == 'Applied machine learning']
+    data = data[data['category'] == sub_category_name]
     pages = list(zip(data.page, data.Predicted_Quality))
-    return render_template('applied_ml.html', pages = pages)
+    return render_template('sub_category.html', pages=pages, sub_category_name=sub_category_name)
 
 
 if __name__ == '__main__':
