773f17e4d7a800ef325669ae5afd579ebfd579b0
Austin Krauss <austin.f.krauss@gmail.com>
2018-08-28 08:42:59 -0700

    Enhance iterateive capabilities for website

--- my_jinja_app/wiki_app.py
+++ my_jinja_app/wiki_app.py
@@ -12,14 +12,14 @@ app = Flask(__name__,
 
 @app.route('/', methods=['GET'])
 def welcome_page():
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 @app.route('/category/<category_name>', methods=['GET'])
 def load_category(category_name):
     data = pickle.load( open( "/Users/austin/Documents/Galvanize/Capstone/Wikipedia_Knowledge_Graph/EDA/ML_data.pkl", "rb" ) )
     data['url_cat'] = data['category'].apply(urllib.parse.quote)
-    return render_template('index.html', data=data, category_name=category_name)
+    return render_template('category.html', data=data, category_name=category_name)
 
 @app.route('/sub_category/<sub_category_name>', methods=['GET'])
 def load_sub_category(sub_category_name):
