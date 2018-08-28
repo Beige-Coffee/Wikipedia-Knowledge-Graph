6742e4504a2a60b802b3bc86604a3cc2c2c118b2
Austin Krauss <austin.f.krauss@gmail.com>
2018-08-28 13:29:38 -0700

    Upload table and internal links

--- my_jinja_app/wiki_app.py
+++ my_jinja_app/wiki_app.py
@@ -19,14 +19,15 @@ def welcome_page():
 def load_category(category_name):
     data = pickle.load( open( "/Users/austin/Documents/Galvanize/Capstone/Wikipedia_Knowledge_Graph/EDA/ML_data.pkl", "rb" ) )
     data['url_cat'] = data['category'].apply(urllib.parse.quote)
-    return render_template('category.html', data=data, category_name=category_name)
+    pages = list(zip(data['category'], data['Predicted_Quality']))
+    return render_template('category.html', data=data, pages=pages, category_name=category_name)
 
 @app.route('/sub_category/<sub_category_name>', methods=['GET'])
 def load_sub_category(sub_category_name):
     data = pickle.load( open( "/Users/austin/Documents/Galvanize/Capstone/Wikipedia_Knowledge_Graph/EDA/ML_pages.pkl", "rb" ) )
     data = data[data['category'] == sub_category_name]
     pages = list(zip(data.page, data.Predicted_Quality))
-    return render_template('sub_category.html', pages=pages, sub_category_name=sub_category_name)
+    return render_template('sub_category.html', data=data, sub_category_name=sub_category_name)
 
 
 if __name__ == '__main__':
