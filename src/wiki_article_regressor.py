aedeaf20bddf7785307f84b7c1e257538118641a
Austin Krauss <34213201+Beige-Coffee@users.noreply.github.com>
2018-09-15 12:37:35 -0700

    Update wiki_article_regressor.py

--- src/wiki_article_regressor.py
+++ src/wiki_article_regressor.py
@@ -6,13 +6,7 @@ import pickle
 from ast import literal_eval
 from textstat.textstat import textstat
 from gensim.corpora import wikicorpus
-from sklearn.ensemble import RandomForestRegressor
-import nltk
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from feature_engineering import feature_engineering
-from sklearn.metrics import mean_squared_error
 
 
 def import_data_from_tsv(tsv_file):
